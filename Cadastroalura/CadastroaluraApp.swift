ef8ea19c64dfd595112999eef084679081009d99
Amanda Paiva <80292119+amandakpaiva@users.noreply.github.com>
2022-03-16 18:05:46 -0300

    Add files via upload

---
+++ Cadastroalura/CadastroaluraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadastroaluraApp.swift
+//  Cadastroalura
+//
+//  Created by Amanda Karolina Santos da Fonseca Paiva on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CadastroaluraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
